8d44928c282d37835711e1aa1fd0783822929186
Wrostdevil <shaktidey2023@gmail.com>
2026-01-21 07:15:41 +0530

    component switch

--- prop/src/App.jsx
+++ prop/src/App.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
-
+import Card from './components/card.jsx'
 const App = () => {
   return (
-    <div className='parent'>
-      <div className="card">
-        <img src='https://images.unsplash.com/photo-1743105351262-3f9e6944920a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
-        <h1>Shakti Prosad Dey
-          </h1>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo, accusamus. 
-          </p>
-          <button>View Profile</button>
-        </div>
-    </div>
+    <Card/>
+    
   )
 }
 
---
+++ prop/src/components/card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div className='parent'>
+      <div className="card">
+        <img src='https://images.unsplash.com/photo-1743105351262-3f9e6944920a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
+        <h1>Shakti Prosad Dey
+          </h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo, accusamus. 
+          </p>
+          <button>View Profile</button>
+        </div>
+    </div>
+  )
+}
+
+export default card
