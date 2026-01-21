d1479bf6676bd90e3f6fe6ddb093a1e763804e0d
Wrostdevil <shaktidey2023@gmail.com>
2026-01-21 07:12:36 +0530

    profile card

--- prop/src/App.jsx
+++ prop/src/App.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <div className="card">I am shakti Prosad Dey</div>
+    <div className='parent'>
+      <div className="card">
+        <img src='https://images.unsplash.com/photo-1743105351262-3f9e6944920a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
+        <h1>Shakti Prosad Dey
+          </h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo, accusamus. 
+          </p>
+          <button>View Profile</button>
+        </div>
     </div>
   )
 }
