7f0e87ab2ecbae8e81b1ca6f3c80685b838b7bd0
Wrostdevil <shaktidey2023@gmail.com>
2026-01-21 06:36:47 +0530

    components done

--- 02.components/src/App.jsx
+++ 02.components/src/App.jsx
@@ -1,7 +1,14 @@
-
+import React from 'react'
+import Card from './components/card'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1>Shakti Prosad Dey</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero distinctio iure placeat ab est facilis consequuntur corrupti dicta aliquid quam, asperiores enim voluptas recusandae blanditiis possimus eligendi neque pariatur quos!</p>
+      <Navbar/>
+      <Card/>
+    </div>
   )
 }
 
---
+++ 02.components/src/components/card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+        <h2>This is a cardd component....</h2>
+    </div>
+  )
+}
+
+export default card
