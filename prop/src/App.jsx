bbf2d22e2dcd01d681b555e07e9e77910fc5972f
Wrostdevil <shaktidey2023@gmail.com>
2026-01-21 19:37:37 +0530

    card trial

--- prop/src/App.jsx
+++ prop/src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import Card from './components/card.jsx'
 const App = () => {
   return (
-    <Card/>
-    
+    <div className='parent'>
+    <Card a='https://images.unsplash.com/photo-1743105351262-3f9e6944920a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'user="Shakti" age={18}/>
+    <Card a='https://plus.unsplash.com/premium_photo-1766436243777-9e3771e2421e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'user="Shuvom" age={12}/>
+    <Card a='https://images.unsplash.com/photo-1768463852017-921060ff24fe?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'user="Aslam" age={18}/>
+    </div>
   )
 }
 
--- prop/src/components/card.jsx
+++ prop/src/components/card.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const card = () => {
+const card = (props) => {
+  console.log(props);
+  console.log(props.user);
+  console.log(props.age);
+
   return (
-    <div className='parent'>
       <div className="card">
-        <img src='https://images.unsplash.com/photo-1743105351262-3f9e6944920a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
-        <h1>Shakti Prosad Dey
-          </h1>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo, accusamus. 
-          </p>
-          <button>View Profile</button>
+        <img src={props.a}></img>
+        <h1>{props.user}</h1>
+          <p>I am  b.tech 3rd year student and {props.age} years old.</p>
+          <button >View Profile</button>
         </div>
-    </div>
   )
 }
 
